7a2eec03a431bd3cbdfc50278b8a15261cd55968
Pedro-Lucas-Leal <104956862+Pedro-Lucas-Leal@users.noreply.github.com>
2022-09-28 20:03:27 -0300

    Update Lista 2 ex 5.py

--- Lista 2 ex 5.py
+++ Lista 2 ex 5.py
@@ -2,7 +2,7 @@ listaInteiros = []
 x = 1
 
 while x <= 10:
-    entradaDados = int(input(f'Digite o {x}º número:'))
+    entradaDados = int(input(f'Digite o {x}º número: '))
     entradaDados = entradaDados * entradaDados
     listaInteiros.append(entradaDados)
     x = x + 1
