28808dc616571b9cda34351bb705038867c16fb1
Pedro-Lucas-Leal <104956862+Pedro-Lucas-Leal@users.noreply.github.com>
2022-09-28 01:35:33 -0300

    Update Lista 1 ex 6.py

--- Lista 1 ex 6.py
+++ Lista 1 ex 6.py
@@ -5,4 +5,3 @@ velocidadeInternet = float(input('Agora digite a velocidade em Mbps '))
 tempo = ((velocidadeInternet/8)*60) / tamanhoArquivo
 tempo = math.ceil(tempo)
 print(f'O seu download levará aproximadamente {tempo:.0f} minutos para ser concluído')
-1
